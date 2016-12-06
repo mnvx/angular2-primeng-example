91034ecbc90a1a739fe50b76b8111b5968ea96cc
mnv <mnvx@yandex.ru>
2016-12-06 23:39:10 +0300

    fix #1

--- client/src/app/index.ts
+++ client/src/app/index.ts
@@ -2,7 +2,7 @@ import {bootstrap} from 'angular2/platform/browser'
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {FORM_PROVIDERS} from 'angular2/common';
 import {ROUTER_PROVIDERS} from 'angular2/router';
-import {LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {LocationStrategy, HashLocationStrategy} from 'angular2/platform/common';
 import {provide} from 'angular2/core';
 import {App} from './app'
 import {Config} from './config';
